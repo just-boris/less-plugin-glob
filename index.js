a7ca555f19501b9b6e47af50f640133e105b0fe9
just-boris <just-boris@yandex-team.ru>
2015-02-11 17:16:16 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,58 @@
+var Promise = typeof Promise === 'undefined' ? require('promise') : Promise,
+    promisify = require('promisify-node'),
+    globby = require('globby'),
+    path = require('path'),
+    eol = require('os').EOL,
+    globbyPromise = promisify(globby);
+
+function isLess(file) {
+    return path.extname(file) === '.less';
+}
+
+function processPaths(paths) {
+    return paths.filter(function(path) {
+        if(!isLess(path)) {
+            console.warn('Here is non-less file: ' + path + ', ignored');
+            return false;
+        }
+        return true;
+    });
+}
+
+module.exports = {
+    install: function(less, pluginManager) {
+        function GlobFileManager() {
+
+        }
+        GlobFileManager.prototype = new less.FileManager();
+        GlobFileManager.prototype.constructor = GlobFileManager;
+        GlobFileManager.prototype.supports = function(filename, currentDirectory, options, environment) {
+            return (filename).indexOf('*') > -1;
+        };
+        GlobFileManager.prototype.loadFile = function(filename, currentDirectory, options, environment) {
+            var self = this;
+            return Promise.all(options.paths.map(function(basePath) {
+                return globbyPromise(path.join(currentDirectory, filename), {cwd: basePath});
+            })).then(function(paths) {
+                paths = Array.prototype.concat.apply([], paths);
+                paths = processPaths(paths);
+                return Promise.all(paths.map(function(file) {
+                    return less.FileManager.prototype.loadFile.call(self, file, currentDirectory, options, environment);
+                }));
+            }).then(function(files) {
+                return {contents: files.map(function(file) {
+                    return file.contents;
+                }).join(eol), filename: filename};
+            });
+        };
+        GlobFileManager.prototype.loadFileSync = function(filename, currentDirectory, options, environment, encoding) {
+            var paths = globby.sync(filename, {cwd: currentDirectory});
+            paths = processPaths(paths);
+            return paths.map(function(file) {
+                return less.FileManager.prototype.loadFileSync.call(this, path.basename(file), path.dirname(file), options, environment, encoding);
+            }, this).join(eol);
+        };
+        GlobFileManager.prototype.supportsSync = GlobFileManager.prototype.supports;
+        pluginManager.addFileManager(new GlobFileManager());
+    }
+};
---
+++ test/index.spec.js
@@ -0,0 +1,58 @@
+var lessGlob = require('../'),
+    less = require('less'),
+    fs = require('fs'),
+    expect = require('chai').expect;
+
+function readResource(filename) {
+    return fs.readFileSync(filename, 'utf-8');
+}
+
+function assertFilesToBeIncluded(output, files) {
+    var pattern = "file:([\\w\\.\\-]+)",
+        includes = output.match(new RegExp(pattern, 'g'));
+    includes.forEach(function(include) {
+        include = new RegExp(pattern).exec(include)[1];
+        expect(files).to.contain(include);
+        files.splice(files.indexOf(include), 1);
+    });
+    expect(files).to.have.length(0, 'all includes should be found');
+}
+
+var options = {
+        paths: ['test/fixtures'],
+        plugins: [lessGlob]
+    };
+
+describe('less-glob', function() {
+    it('should import files by glob', function(done) {
+        less.render(readResource('test/fixtures/all-less.less'), options)
+            .then(function(output) {
+                assertFilesToBeIncluded(output.css, [
+                    'one.less',
+                    'one-sub.less'
+                ]);
+            })
+            .then(done, done);
+    });
+
+    it('should ignore non-less files', function(done) {
+        less.render(readResource('test/fixtures/include-non-less.less'), options)
+            .then(function(output) {
+                assertFilesToBeIncluded(output.css, [
+                    'two.less'
+                ]);
+                expect(output.css).to.not.contain('file:resource.txt');
+            })
+            .then(done, done);
+    });
+
+    it('should not break standard imports', function(done) {
+        less.render(readResource('test/fixtures/no-glob.less'), options)
+            .then(function(output) {
+                assertFilesToBeIncluded(output.css, [
+                    'one.less'
+                ]);
+            })
+            .then(done, done);
+    });
+});
